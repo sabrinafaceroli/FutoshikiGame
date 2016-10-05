10c842de838e79f6b51a7f5474bebab9a959ec05
Sabrina <sabrina.tridico@usp.br>
2016-10-05 11:18:46 -0300

    Leitura de dados iniciais

---
+++ projeto.py
@@ -0,0 +1,20 @@
+# Trabalho 1 de Algoritmos Avancados
+# Sabrina Faceroli - 9066452
+# Joao Gabriel Fontana - 
+
+# THE AMAZING AND MOST AWESOME PYTHON FUTOSHIKI EVER!
+# -----------------------------------------------------
+
+# Leitura dos dados
+casosdeteste = input()
+tamanhoJogo, qtsRestr = raw_input().split(" ")
+
+#Definindo a matriz do jogo
+jogo = [[0 for a in range(tamanhoJogo)] for b in tamanhoJogo]
+
+# Lendo as linhas do jogo
+for x in range(0, tamanhoJogo) 
+    for y in range(0, tamanhoJogo)
+        jogo[x][y] = raw_input().split(" ") #Nao eh assim que se usa o input, checar isso depois
+
+
